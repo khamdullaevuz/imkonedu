818d1dae5a5c7c92e1b8cd4d28237ecc50064ce6
Elbek Khamdullaev <elbek.khamdullaev@mail.ru>
2021-06-21 21:31:22 +0500

    ok

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
 
-$db = @mysqli_connect("localhost", "elbek", "1529", "imkonedu") or die("Ulanishda xatolik");
+$db = @mysqli_connect("localhost", "u1371402_default", "5ByzE53AdIagM3K6xX2o", "u1371402_Telegrambot") or die("Ulanishda xatolik");
 @mysqli_set_charset($db, "utf8mb4") or die("Kodirovka xatoligi");
